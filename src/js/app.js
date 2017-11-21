96ac9d6d12c56a81fbf371e0946a548a1aa67820
Keith Rodriguez <rodriguez.keith@gmail.com>
2017-11-20 23:33:04 -0600

    Basic functionality

--- src/js/app.js
+++ src/js/app.js
@@ -2,39 +2,65 @@ import axios from 'axios';
 import 'normalize.css/normalize.css';
 import '../styles/styles.scss';
 
-// ===============================
-// IS THERE A UNITS OPTION IN API?
-const fahrenheit = true; // use ternary operators for ? temp : (temp - 32) * 5 / 9;
-// ===============================
+const unitSelector = document.querySelector('.unit-selector');
+const currentTemp = document.querySelector('.current-temp');
+// const currentWeath = currentTemp.querySelector('.wi');
+let latitude;
+let longitude;
+let units = JSON.parse(localStorage.getItem('units')) || 'us';
+console.log(units);
 
 // app icons - use a default
-// clear-day
-// clear-night
-// rain
-// snow
-// sleet
-// wind
-// fog
-// cloudy
-// partly-cloudy-day
-// partly-cloudy-night
+const icons = {
+  'clear-day': 'day-sunny',
+  'clear-night': 'night-clear',
+  'rain': 'rain',
+  'snow': 'snow',
+  'sleet': 'sleet',
+  'wind': 'strong-wind',
+  'fog': 'fog',
+  'cloudy': 'cloudy',
+  'partly-cloudy-day': 'day-cloudy',
+  'partly-cloudy-night': 'night-alt-cloudy'
+}
+
+
+function updateData(res) {
+  currentTemp.innerHTML = `${Math.round(res.data.currently.temperature)}&deg;<i class="wi wi-${icons[res.data.currently.icon]}"></i>`;
+}
 
 function fetchData(lat, long) {
-  const url = `https://crossorigin.me/https://api.darksky.net/forecast/ae4574e6f3db656bc32a6df7cf73842c/${lat},${long}`;
+  console.log('Fetching Data...');
+  // find a better solution for No 'Access-Control-Allow-Origin' header
+  const url = `https://crossorigin.me/https://api.darksky.net/forecast/ae4574e6f3db656bc32a6df7cf73842c/${lat},${long}?units=${units}`;
   axios.get(url)
     .then((res) => {
-      console.log('Data!!!', res);
+      console.log('Data!!!', res.data);
+      updateData(res);
     })
     .catch((e) => {
       console.log('Error!!!', e);
     });
 }
 
-navigator.geolocation.getCurrentPosition((data) => {
-  const { latitude, longitude } = data.coords;
-  // console.log(latitude, longitude);
-
+function setUnits() {
+  units = units === 'us' ? 'si' : 'us';
+  localStorage.setItem('units', JSON.stringify(units));
+  unitSelector.textContent = `${units === 'us' ? 'F' : 'C'}`;
   fetchData(latitude, longitude);
+}
+
+unitSelector.addEventListener('click', setUnits);
+
+navigator.geolocation.getCurrentPosition((data) => {
+  // { latitude, longitude } = data.coords;
+  latitude = data.coords.latitude;
+  longitude = data.coords.longitude;
+  console.log(latitude, longitude);
+
+  setUnits();
+  // fetchData(latitude, longitude); // moved to setUnits function - delete
 }, (e) => {
   alert(e, 'We need your location to fetch your weather');
+  // run different location service
 });
