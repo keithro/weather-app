2a4ec6cce763ab9a329031d19d8a31ef8a773040
Keith Rodriguez <rodriguez.keith@gmail.com>
2017-11-28 15:05:19 -0600

    Mobile styling

--- src/js/app.js
+++ src/js/app.js
@@ -2,33 +2,39 @@ import axios from 'axios';
 import 'normalize.css/normalize.css';
 import '../styles/styles.scss';
 
-const { updateCurrentWeather } = require('./utils/update-data');
+const { updateWeather } = require('./utils/update-data');
 
 const body = document.querySelector('body');
 const unitSelector = document.querySelector('.unit-selector');
-
+let units = JSON.parse(localStorage.getItem('units')) || 'us';
 let latitude;
 let longitude;
-// const weather = {};
-let units = JSON.parse(localStorage.getItem('units')) || 'us';
+let weatherData;
 
 function fetchData(lat, long) {
   console.log('Fetching Data...');
   // find a better solution for No 'Access-Control-Allow-Origin' header
   const url = `https://crossorigin.me/https://api.darksky.net/forecast/ae4574e6f3db656bc32a6df7cf73842c/${lat},${long}?units=${units}`;
+  // const url = `https://cors-anywhere.herokuapp.com/ https://api.darksky.net/forecast/ae4574e6f3db656bc32a6df7cf73842c/${lat},${long}?units=${units}`;
 
-  // Current Weather AJAX Call
   axios.get(url)
     .then((res) => {
       // ADD WEATHER DATA TO LOCAL STORAGE FOR FALL BACK IF NOT WORKING
+      localStorage.setItem('weatherData', JSON.stringify(res.data));
+      updateWeather(res.data);
       body.classList.toggle('ready');
-      updateCurrentWeather(res.data);
     })
     .catch((e) => {
       console.log('Error!!!', e);
+      // use data in local storage
+      weatherData = JSON.parse(localStorage.getItem('weatherData'));
+      updateWeather(weatherData);
+      body.classList.toggle('ready');
+      // append error message to h2
     });
 }
 
+// *** MOVE TO UPDATE-DATA FILE ***
 function setUnits() {
   localStorage.setItem('units', JSON.stringify(units));
   unitSelector.textContent = `${units === 'us' ? 'F' : 'C'}`;
@@ -57,5 +63,7 @@ runApp();
 
 // Simulate AJAX call to test styling, animation
 // setTimeout(() => {
+//   weatherData = JSON.parse(localStorage.getItem('weatherData'));
+//   updateWeather(weatherData);
 //   body.classList.toggle('ready');
 // }, 1000);
--- src/js/utils/update-data.js
+++ src/js/utils/update-data.js
@@ -9,15 +9,39 @@ const windDisplay = document.querySelector('.wind-display > span');
 const dateHeadings = document.querySelectorAll('.date-heading');
 const dailyForecasts = document.querySelectorAll('.daily-forecast');
 
-const updateCurrentWeather = (data) => {
+const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+const time = new Date();
+const day = time.getDay();
+// const date = time.getDate();
+
+const updateWeather = (data) => {
   console.log('Data!!!', data);
 
+  // Current Weather Content
   summary.textContent = `Currently ${data.currently.summary}`; // `${data.weather[0].main} in ${data.name}`;
   currentTemp.innerHTML = `${Math.round(data.currently.temperature)}&deg; <i class="wi wi-forecast-io-${data.currently.icon}"></i>`;
-  highLow.textContent = `${Math.round(data.daily.data[0].temperatureHigh)}°/${Math.round(data.daily.data[0].temperatureLow)}°`;
+  highLow.innerHTML = `${Math.round(data.daily.data[0].temperatureHigh)}°<span></span>${Math.round(data.daily.data[0].temperatureLow)}°`;
   rainDisplay.textContent = `${Math.round(data.currently.precipProbability * 100)}`;
   windDisplay.innerHTML = `${Math.round(data.currently.windSpeed)} <i class="wi wi-wind from-${windIcon(data.currently.windBearing)}-deg"></i>`;
 
+  // Forecast Content
+  dateHeadings.forEach((heading, ind) => {
+    heading.textContent = `${day + ind + 1 < 7 ? days[day + ind + 1] : days[day + ind - 6]}`;
+  });
+
+  dailyForecasts.forEach((forecast, ind) => {
+    const dailyData = data.daily.data[ind + 1];
+    forecast.innerHTML = `
+      <div class="daily-temp">
+        <p>${Math.round(dailyData.temperatureHigh)}°</p>
+        <p>${Math.round(dailyData.temperatureLow)}°</p>
+      </div>
+      <i class="wi wi-forecast-io-${dailyData.icon}"></i>
+      <p><i class="wi wi-raindrops"></i> ${Math.round(dailyData.precipProbability * 100)}%</p>
+    `;
+  });
+
+  // Background
   // // set background based on data.weather.description
   // console.log('weather id', data.weather[0].id);
   // if (data.weather[0].icon.includes('n')) { // this is alrady the default bg
@@ -33,24 +57,6 @@ const updateCurrentWeather = (data) => {
   // setTimeout(() => {             // Do I need this for background transition?
 
   // }, 100);
-
-  const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-  const time = new Date();
-  const day = time.getDay();
-  // const date = time.getDate();
-  dateHeadings.forEach((heading, ind) => {
-    heading.textContent = `${day + ind + 1 < 7 ? days[day + ind + 1] : days[day + ind - 6]}`;
-  });
-  
-  dailyForecasts.forEach((forecast, ind) => {
-    const dailyData = data.daily.data[ind + 1];
-    console.log(dailyData);
-    forecast.innerHTML = `
-      <h3>${Math.round(dailyData.temperatureHigh)}°/${Math.round(dailyData.temperatureLow)}°</h3>
-      <h3><i class="wi wi-forecast-io-${dailyData.icon}"></i></h3>
-      <h3>${Math.round(dailyData.precipProbability * 100)}%</h3>
-    `;
-  });
 };
 
-module.exports = { updateCurrentWeather };
+module.exports = { updateWeather };
